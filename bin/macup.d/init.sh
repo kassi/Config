b76e0f6e989f78d0231300e2e322f2117e8d9c34
Karsten Silkenbäumer <kassi@users.noreply.github.com>
2020-07-28 10:30:26 +0200

    Add waiting for xcode-select to finish

--- bin/macup.d/init.sh
+++ bin/macup.d/init.sh
@@ -2,8 +2,8 @@
 set -e
 
 destination=${1:-System/Config}
-if [ -d $destination ]; then
-  if [ -d $destination/.git ]; then
+if [[ -d $destination ]]; then
+  if [[ -d $destination/.git ]]; then
     echo "Destination already contains a git repository. Aborting"
     exit 1
   else
@@ -11,8 +11,14 @@ if [ -d $destination ]; then
   fi
 fi
 
+# Command Line Developer Tools are needed for git to clone…
 echo "Installing Xcode Command Line Tools."
-xcode-select --install
+while [[ $((xcode-select --install) 2>&1) =~ "xcode-select: note: " ]]; do
+  while [[ -n $(ps ax | grep "Install Command Line Developer Tools.app" | grep -v grep) ]]
+  do
+    sleep 10
+  done
+done
 
 echo "Cloning into $destination."
 git clone --bare git@github.com:kassi/Config $destination
